f5b4ef026ec8f62784372c8df7e60c969f16b4ad
Adam <adamst@list.ru>
2019-10-01 17:53:15 +0300

    тестирование коммита

---
+++ teach1/script.js
@@ -0,0 +1,6 @@
+document.writeln("js work!");
+console.log("test console!");
+//alert("test alert!");
+let arr = ['myfoto.jpg', 'vesna.png', 'system.txt'];
+//let youQust = prompt("Вы знаете ответ?!", "Да");
+console.log(typeof(arr));
